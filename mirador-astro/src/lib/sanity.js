ed8e11c4d45c37775095036da1973058c7096bc2
Jeremy <jeremy@onenetinc.com>
2025-04-21 20:11:37 -0600

    change to use Cdn

--- mirador-astro/src/lib/sanity.js
+++ mirador-astro/src/lib/sanity.js
@@ -5,7 +5,7 @@ dotenv.config();
 export const sanityClient = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: "production",
-  useCdn: false, // Set to false if you need fresh data every time
+  useCdn: true, // Set to false if you need fresh data every time
   apiVersion: "2024-02-01", // Use the latest API version
   perspective: "published",
 });
