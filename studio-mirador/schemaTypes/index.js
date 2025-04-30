dff0c9b5c303ad6f378afec9427ba0a220223546
Jeremy <jeremy@onenetinc.com>
2025-04-29 22:49:05 -0600

    update

--- studio-mirador/schemaTypes/index.js
+++ studio-mirador/schemaTypes/index.js
@@ -3,6 +3,7 @@ import authors from "../schemas/authors";
 import blogs from "../schemas/blogs";
 import categories from "../schemas/categories";
 import socialPosts from "../schemas/socialPosts.jsx";
+// force
 
 
 export const schemaTypes = [testimonials, authors, blogs, categories, socialPosts];
